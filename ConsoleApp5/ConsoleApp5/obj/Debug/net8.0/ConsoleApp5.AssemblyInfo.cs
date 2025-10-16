db012900a915d6cd47555977c2ea68c02eb7191d
Kowaido <beginingdaniretro@gmail.com>
2025-10-16 10:36:22 +0300

    Kowaido
    Вот

--- ConsoleApp5/ConsoleApp5/obj/Debug/net8.0/ConsoleApp5.AssemblyInfo.cs
+++ ConsoleApp5/ConsoleApp5/obj/Debug/net8.0/ConsoleApp5.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b00aafa63483035ec5b808129312d5e7111e6a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
